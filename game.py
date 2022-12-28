0d721958edf0419d9a91df822941a9f913f68d30
Dane Jensen <dhcjensen@gmail.com>
2022-12-27 19:35:33 -0800

    Pylint pass

--- game.py
+++ game.py
@@ -3,6 +3,9 @@ A class to keep track of meta data about the game.
 """
 
 class Game :
+    """
+    Holds meta information about the game. Might see more use with more features.
+    """
     def __init__(self) :
         self.players = []
         self.win_cons = (
@@ -17,9 +20,16 @@ class Game :
         )
 
     def add_player(self, player) :
+        """
+        Adds the player to the game's list.
+        """
         self.players.append(player)
 
     def keep_playing(self, idx) :
+        """
+        Checks the games win conditions against a players moves
+        to determine if a win has happened.
+        """
         for win_con in self.win_cons :
             if win_con == self.players[idx].moves :
                 return False
